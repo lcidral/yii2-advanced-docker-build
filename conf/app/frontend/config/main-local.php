c344ca2bb2b04ef1fa7946c6432813b452a262af
Leonardo Cidral <leonardo.cidral@dotgroup.com.br>
2017-08-28 03:36:22 -0300

    last updates

--- conf/app/frontend/config/main-local.php
+++ conf/app/frontend/config/main-local.php
@@ -1,20 +1,25 @@
 <?php
-return [
+
+$config = [
     'components' => [
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=' . getenv('APP_MYSQL_HOST') . 'dbname=' . getenv('APP_MYSQL_DBNAME'),
-            'username' => 'dbname=' . getenv('APP_MYSQL_USERNAME'),
-            'password' => getenv('APP_MYSQL_PASSWORD'),
-            'charset' => 'utf8',
-        ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'uKuoZytao4OK_FwF3JtezlFp38ujDZCo',
         ],
     ],
 ];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
